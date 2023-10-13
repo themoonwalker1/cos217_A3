344c05ca2066deaef13cbd7fb27784b895f89782
Chinmay Bhandaru <praneethsbhandaru@gmail.com>
2023-10-12 20:53:13 -0400

    fix: mixed declerations and code

--- symtablehash.c
+++ symtablehash.c
@@ -46,7 +46,7 @@ SymTable_T SymTable_new(void)
     SymTable_T oSymTable;
     size_t i;
     const int INITIALIZATION_BUCKETS = 509;
-    struct SymTableNode **ppsFirstNode
+    struct SymTableNode **ppsFirstNode;
 
     oSymTable = (SymTable_T)malloc(sizeof(struct SymTable));
     if (oSymTable == NULL)
